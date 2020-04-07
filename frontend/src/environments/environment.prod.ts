034a150aa9cd4ce3c219bad636f69a068595e735
Lemos <cesaralemos@gmail.com>
2020-04-07 15:30:54 -0300

    feat: definição de uri padrão do sistema

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-233-179-194.sa-east-1.compute.amazonaws.com:4200'
+  apiUrl: 'http://ec2-54-233-179-194.sa-east-1.compute.amazonaws.com:4200/slidemaker/api/v1'
 };
