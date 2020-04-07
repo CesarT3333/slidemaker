4ddfa9e8023bd246fb69cd359cd4727a19fedcba
Lemos <cesaralemos@gmail.com>
2020-04-07 15:31:23 -0300

    feature: definição de recursos

---
+++ frontend/src/app/util/resources.ts
@@ -0,0 +1,9 @@
+const BASE_URI = 'slidemaker/api/v1';
+
+export const resources = {
+  PLANOS: `${BASE_URI}/planos`,
+  AUTH: `${BASE_URI}/auth`,
+  AUTH_USUARIO_LOGADO: `${BASE_URI}/auth/usuario-logado`,
+  APRESENTACOES: `${BASE_URI}/apresentacoes`,
+  USUARIOS: `${BASE_URI}/usuarios`,
+};
