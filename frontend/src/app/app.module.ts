0b0de3d7cac48c9f110e16dfa79bf947896cc9fd
Lemos <cesaralemos@gmail.com>
2020-04-07 17:24:30 -0300

    feat: registro de locale ptbr

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import ptBr from '@angular/common/locales/pt';
 
 import { LoadingComponent } from './components/loading/loading.component';
 import { AppInterceptor } from './services/app.interceptor';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
@@ -23,6 +27,10 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent],
   providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AppInterceptor,
