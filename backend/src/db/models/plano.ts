a8a02947d1b4eb90b19defef8b86b003ec1a6d84
Lemos <cesaralemos@gmail.com>
2020-04-07 15:26:48 -0300

    feat: criação de entidade de plano

---
+++ backend/src/db/models/plano.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'plano' })
+export default class Plano {
+
+    @PrimaryGeneratedColumn()
+    id?: number;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    descricao: string;
+
+    @Column({ name: 'quantitativo_descricao' })
+    quantitativoDescricao: string;
+
+    @Column({ name: 'quantitativo_quantidade' })
+    quantitativoQuantidade: number;
+
+    @Column()
+    valor: number;
+    
+    @Column()
+    atributos: string;
+
+}
