924be65ba53b184404610ad15c957c91c3513530
Lemos <cesaralemos@gmail.com>
2020-04-10 18:43:03 -0300

    feat: criação de componente principal de modulo de apresentaçãoi

---
+++ frontend/src/app/modules/apresentacao/pages/apresentacao/apresentacao.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './apresentacao.component.html',
+  styleUrls: ['./apresentacao.component.scss']
+})
+export class ApresentacaoComponent
+  implements OnInit {
+
+  ngOnInit(): void { }
+
+}
