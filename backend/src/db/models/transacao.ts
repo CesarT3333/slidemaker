a72efbd5df4db4656b196b4d4506bf2d8dbc1dc9
Lemos <cesaralemos@gmail.com>
2020-04-12 01:30:36 -0300

    refactor: transaction module deletion

--- backend/src/db/models/transacao.ts
+++
@@ -1,8 +0,0 @@
-import Plan from './plan';
-import User from './user';
-
-export class Transacao {
-  plano: Plan;
-  usuario: User;
-  quantidadeApresentacoes: number;
-}
--- backend/src/modules/transacao/transacao.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-import { resources } from '../../util/resources';
-
-@Controller(resources.TRANSACOES)
-export class TransacaoController { }
