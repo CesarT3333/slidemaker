e23dfd1843abd5869fba9ccbae4b9c912b040c44
Lemos <cesaralemos@gmail.com>
2020-04-14 11:00:11 -0300

    feat: creating file reader service

--- frontend/src/app/modules/apresentacao/apresentacao.module.ts
+++ frontend/src/app/modules/apresentacao/apresentacao.module.ts
@@ -2,6 +2,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -13,6 +14,7 @@ import { HeaderToolbarComponent } from './components/header-toolbar/header-toolb
 import { ApresentacaoComponent } from './pages/apresentacao/apresentacao.component';
 import { ApresentacaoService } from 'src/app/services/apresentacao.service';
 import { DataSourceService } from 'src/app/services/data-sources.service';
+import { FileReaderService } from 'src/app/services/file-reade.service';
 import { ApresentacaoRoutingModule } from './apresentacao.routing';
 import { IdiomService } from 'src/app/services/idiom.service';
 
@@ -24,6 +26,7 @@ import { IdiomService } from 'src/app/services/idiom.service';
     FormsModule,
 
     MatFormFieldModule,
+    MatSelectModule,
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
@@ -40,6 +43,7 @@ import { IdiomService } from 'src/app/services/idiom.service';
   ],
   providers: [
     ApresentacaoService,
+    FileReaderService,
     DataSourceService,
     IdiomService,
   ]
