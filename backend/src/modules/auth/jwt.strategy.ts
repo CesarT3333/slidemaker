29c6bbdec619f2f488884be5fd1d8c165c065879
Lemos <cesaralemos@gmail.com>
2020-04-15 16:04:53 -0300

    refactor: secret token jwt

--- backend/src/modules/auth/jwt.strategy.ts
+++ backend/src/modules/auth/jwt.strategy.ts
@@ -2,24 +2,25 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 
 import { ExtractJwt, Strategy } from 'passport-jwt';
-
-import * as jwt from '../../credentials/secret-app.json';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: jwt.secret_key,
-        });
-    }
+  constructor(
+    private configService: ConfigService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('SECRET_JWT'),
+    });
+  }
 
-    async validate(payload, done: any) {
-        try {
-            done(null, payload);
-        } catch (err) {
-            throw new UnauthorizedException('unauthorized', err.message);
-        }
+  async validate(payload, done: any) {
+    try {
+      done(null, payload);
+    } catch (err) {
+      throw new UnauthorizedException('unauthorized', err.message);
     }
+  }
 }
