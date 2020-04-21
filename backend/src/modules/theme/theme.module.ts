bfdfc22359603c9b1ad424851f2c655aba72020d
Lemos <cesaralemos@gmail.com>
2020-04-21 11:25:06 -0300

    feat: adding theme repository in dependency injection

--- backend/src/modules/theme/theme.module.ts
+++ backend/src/modules/theme/theme.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 
 import { GoogleDriveService } from '../../services/google-drive.service';
+import { ThemeRepository } from '../../db/repository/theme.repository';
 import { ThemeService } from '../../services/theme.service';
 import { ThemeController } from './theme.controller';
+import { Theme } from '../../db/models/theme';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule, TypeOrmModule.forFeature([Theme, ThemeRepository])
+  ],
   controllers: [ThemeController],
   providers: [
     ThemeService,
