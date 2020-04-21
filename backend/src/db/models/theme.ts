838498c0055672b5185fb0f96a46f600f7a95cf1
Lemos <cesaralemos@gmail.com>
2020-04-21 11:24:19 -0300

    feat: creating theme model/repository

---
+++ backend/src/db/models/theme.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'themes' })
+export class Theme {
+
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column()
+  name: string;
+
+  @Column({ name: 'google_id_img' })
+  googleIdImg: string;
+
+  @Column({ name: 'google_id_presentation' })
+  googleIdPresentation: string;
+
+}
