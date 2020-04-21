40a6595fa8cc97ddebb71a71a25bd0e5f0a3c08d
Lemos <cesaralemos@gmail.com>
2020-04-21 11:20:11 -0300

    chore: deletion app.controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,17 +5,15 @@ import { Module } from '@nestjs/common';
 
 import { join } from 'path';
 
-import { AuthModule } from './modules/auth/auth.module';
+import { UserSignatureModule } from 'modules/user-signature/user-signature.module';
+import { PresentationModule } from 'modules/presentation/presentation.module';
+import { DataSourceModule } from 'modules/data-sources/data-source.module';
+import { IdiomModule } from 'modules/idiom/idiom.module';
+import { ThemeModule } from 'modules/theme/theme.module';
+import { UserModule } from 'modules/usuario/user.module';
+import { PlanModule } from 'modules/plan/plan.module';
+import { AuthModule } from 'modules/auth/auth.module';
 
-import { AppController } from './app.controller';
-
-import { UserSignatureModule } from './modules/user-signature/user-signature.module';
-import { PresentationModule } from './modules/presentation/presentation.module';
-import { DataSourceModule } from './modules/data-sources/data-source.module';
-import { ThemeModule } from './modules/theme/theme.module';
-import { UserModule } from './modules/usuario/user.module';
-import { IdiomModule } from './modules/idiom/idiom.module';
-import { PlanModule } from './modules/plan/plan.module';
 import database from './config/database';
 
 @Module({
@@ -37,10 +35,6 @@ import database from './config/database';
     UserModule,
     PlanModule,
     AuthModule,
-
-  ],
-  controllers: [
-    AppController
   ]
 })
 export class AppModule { }
