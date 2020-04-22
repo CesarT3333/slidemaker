d814c3065eddefc8f3aa5e06715f53a5700de44e
Lemos <cesaralemos@gmail.com>
2020-04-22 14:35:26 -0300

    fix: deploy

--- backend/src/modules/theme/theme.controller.ts
+++ backend/src/modules/theme/theme.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { ThemeService } from '../../services/theme.service';
